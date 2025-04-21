2c90c3bf40fdc927c453a08ed4f29c7d61f1e679
Elias Rodkey <elias.rodkey@gmail.com>
2025-04-20 20:18:11 -0700

    Adds funcitonality to log handler functions and creates ELoggingFormat class for centralized log format storage

--- packages/loggers/loggers/common.py
+++ packages/loggers/loggers/common.py
@@ -6,31 +6,83 @@ A python module which contains generic functions related to the logger and super
 functions:
     
 """
-import logging
+from datetime import datetime
+from enum import Enum
 from typing import List, Dict, Union
 
-# Configure logging (best practice for debugging and monitoring)
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-
-def create_log_filename(run_id: str) -> str:
+class ELoggingFormats(str, Enum):
     """
-    a function that uses current datetime and program run id to create a new log filename
+    Contains different format strings for logging output
+    """
+    ASCTIME = '%(asctime)s'
+    MODULE = '%(module)s'
+    MODULE_BRACKETS = '[%(module)s]'
+    FUNC_NAME = '%(funcName)s'
+    FUNC_NAME_BRACKETS = '[%(funcName)s]'
+    LOG_LEVEL = '%(levelname)s'
+    LOG_LEVEL_BRACKETS = '[%(levelname)s]'
+    LINE_NO = '%(lineno)d'
+    LINE_NO_BRACKETS = '[%(lineno)d]'
+    LOGGER_NAME = '%(name)s'
+    LOGGER_NAME_BRACKETS = '[%(name)s]'
+    MESSAGE = '%(message)s'
+
+    FORMAT_BASIC = f'{ASCTIME} - {LOG_LEVEL} - {MESSAGE}'
+    FORMAT_LOGGER_NAME = f'{ASCTIME} - {LOGGER_NAME} - {LOG_LEVEL} - {MESSAGE}'
+    FORMAT_LOGGER_NAME_BRACKETS = f'{ASCTIME} - {LOGGER_NAME_BRACKETS}{LOG_LEVEL_BRACKETS}: {MESSAGE}'
+    FORMAT_FUNC_NAME = f'{ASCTIME} {LOG_LEVEL_BRACKETS}{FUNC_NAME_BRACKETS}: {MESSAGE}'
+    FORMAT_MODULE_FUNC_NAME = f'{ASCTIME} {LOG_LEVEL_BRACKETS}{MODULE_BRACKETS}{FUNC_NAME_BRACKETS}: {MESSAGE}'
+
+    def __str__(self):
+        return str(self.value)
+
+
+
+def create_datestamp() -> str:
+    """
+    Creates a string of the current datetime YYYY-MM-DD
 
     Args:
-        run_id (str): A string which defines a way to identify the program run
+        None
+    
+    Returns:
+        the current days date as a string    
+    """
+    return datetime.now().strftime('%Y-%m-%d')
+
+
+def create_timestamp() -> str:
+    """
+    Creates a string of the current time HHMMSS
+
+    Args:
+        None
+    
+    Returns:
+        the current time as a string    
+    """
+    return datetime.now().strftime('%H%M%S')
+
+
+def create_log_datetime_stamp() -> str:
+    """
+    Uses current datetime and program run id to create a datetime stamp for log file
+
+    Args:
+        None
 
     Returns:
-        str: a log filename ending in .log
+        str: a log filename datetime stamp
     """
-    pass
+    return f'{create_datestamp()}_{create_timestamp()}'
 
 
 if __name__ == "__main__":
-    # Example usage and testing
-    logging.info("Executing module_template.py as a standalone script.")
-
-
     sample_data = [10, 20, 30, 40, 50]
-    results = create_log_filename()
-    print(results)
+    date = create_datestamp()
+    time = create_timestamp()
+    datetime_stamp = create_log_datetime_stamp()
+    print(date)
+    print(time)
+    print(datetime_stamp)
--- packages/loggers/loggers/logger.py
+++ packages/loggers/loggers/logger.py
@@ -7,20 +7,29 @@ Class:
 import logging
 from typing import List, Dict, Union
 
-# Configure logging (best practice for debugging and monitoring)
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
+from common import ELoggingFormats
 
 class Logger:
     """
     A logging class that uses singleton-like behavior to avoid creating duplicate loggers.
     Allows changing logging output location and level
     """
+
     # Initialize instances dictionary
     _instances = {}
+    LOG_FILE_DEFAULT_DIRECTORY: str = 'docs\logs'
+
+    DEBUG = logging.DEBUG
+    INFO = logging.INFO
+    WARN = logging.WARN
+    ERROR = logging.ERROR
+    CRITICAL = logging.CRITICAL
 
 
-    def __new__(cls, name, level=logging.INFO, log_to_file: bool=False):
+    def __new__(
+            cls, name: str,
+            log_file_default_dir: str = LOG_FILE_DEFAULT_DIRECTORY,
+            ):
         """
         creates memory for the new object before __init__() is called. used in this case to control instance creation
 
@@ -28,70 +37,91 @@ class Logger:
             cls: refers to the class itself, always an argument in the __new__ method for classes
                 (like self for __init__ method)  
             name (str): the name of the logger instance being created, will be added to _instances{}
-            level: sets the output logging level for the logger at creation, can be changed during runtime using
-                logger.setLevel(logger.LEVEL)
-            log_to_file (bool): default False. if False no file handler created, if True automatic log file created using standard format
+            log_file_default_dir : default relative location for log files to be generated 
         
         Returns:
             None
         """
-        # TODO: Add descriptions of what each code block does here
+
+        # Checks if the logger name already exists in the _instances dictionary
+        # If the name exists it retrieves the logger that is already created
         if name in cls._instances:
             return cls._instances[name]
         
+        # Calls the default __new__ method to create a new instance of the class
         instance = super(Logger, cls).__new__(cls)
+
+        # Retrieves or creates a names logger instance
         instance.logger = logging.getLogger(name)
-        instance.logger.setLevel(level)
-
-        # Console handler
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-        instance.logger.addHandler(console_handler)
-
-        # File handler (if specified)
-        if log_to_file:
-            # TODO: Create a automatic log file generation in the docs/logs folder
-            file_handler = logging.FileHandler(log_to_file)
-            file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-            instance.logger.addHandler(file_handler)
 
         cls._instances[name] = instance
         return instance
 
 
+    def __init__(
+            self, name: str,
+            log_file_default_dir: str = LOG_FILE_DEFAULT_DIRECTORY
+            ):
+        # Initialize instance level variables
+        self.name = name
+        self.log_file_defaullt_dir = log_file_default_dir
+
+
     def get_logger(self):
+        """Retrives the new logger instance"""
         return self.logger
 
 
+    def add_console_handler(
+            self, level = logging.INFO, 
+            format = ELoggingFormats.FORMAT_BASIC
+            ) -> None:
+        """
+        Creates a console log handler for the current logger instance
 
+        Args:
+            level: logging display level
+            format (str): logging message display format
+        
+        Returns:
+            None
+        """
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(logging.Formatter(format))
+        console_handler.setLevel(level)
+        self.logger.addHandler(console_handler)
+        
 
-def sample_function(data: List[int]) -> Dict[str, Union[int, float]]:
-    """
-    A sample function that performs basic calculations on a list of numbers.
+    
+    def add_file_handler(
+            self, level = logging.INFO, 
+            format = ELoggingFormats.FORMAT_BASIC
+            ) -> None:
+        """
+        Creates a file log handler for the current logger instance
 
-    Args:
-        data (List[int]): A list of integers.
-
-    Returns:
-        Dict[str, Union[int, float]]: A dictionary containing calculated metrics.
-    """
-    if not data:
-        logging.warning("Data list is empty.")
-        return {"count": 0, "average": 0.0}
-
-    return {
-        "count": len(data),
-        "average": sum(data) / len(data)
-    }
+        Args:
+            level: logging display level
+            format (str): logging message display format
+        
+        Returns:
+            None
+        """
+        # TODO: automatically create path / dir to new log file
+        file_name = ''
+        file_handler = logging.FileHandler(filename = file_name, mode = 'w', encoding = 'utf-8')
+        file_handler.setFormatter(logging.Formatter(format))
+        file_handler.setLevel(level)
+        self.logger.addHandler(file_handler)
 
 
 if __name__ == "__main__":
-    # Example usage and testing
-    logging.info("Executing module_template.py as a standalone script.")
 
-    instance = Logger("test")
-    print(instance.display())
+    test_instance = Logger("test", level = logging.DEBUG)
+    main_instance = Logger("main", logging_format=ELoggingFormats.FORMAT_LOGGER_NAME_BRACKETS)
 
-    sample_data = [10, 20, 30, 40, 50]
-    results = sample_function(sample_data)
-    print(results)
+    test_logger = test_instance.get_logger()
+    main_logger = main_instance.get_logger()
+
+    test_logger.debug('this is the test logger')
+    main_logger.info('this is the main logger')
