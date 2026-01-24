36cc21585ff5a6378e92f5ed17bae93aaa75fbf0
Elias Rodkey <Elias.Rodkey@gmail.com>
2026-01-23 19:42:35 -0800

    Updates verioning in __init__.py and pyproject.toml files.

--- packages/local_db/tests/__init__.py
+++ packages/local_db/tests/__init__.py
@@ -1,9 +1,9 @@
 #!python3
-'''
+"""
 local_db_handler.tests
 
 This package contains the test scripts for the local_db_handler package
-'''
+"""
 # Metadata
-__version__ = '0.2.0'
-__author__ = 'Elias Rodkey'
+__version__ = "1.1.0"
+__author__ = "Elias Rodkey"
