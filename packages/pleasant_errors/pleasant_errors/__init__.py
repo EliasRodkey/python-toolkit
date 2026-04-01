678b6eadf8c83d24c9e955dbd259f6c7bcf59e95
EliasRodkey <66490689+EliasRodkey@users.noreply.github.com>
2026-03-31 21:29:13 -0700

    Updates README.md for pleasant_errors. upversions to 1.0.

--- packages/pleasant_errors/pleasant_errors/__init__.py
+++ packages/pleasant_errors/pleasant_errors/__init__.py
@@ -3,7 +3,7 @@ from pleasant_errors.models import Error, StructuredError
 from pleasant_errors.result import Err, Ok, Result
 
 # Metadata
-__version__ = "1.3.3"
+__version__ = "1.0.0"
 __author__ = "Elias Rodkey"
 
 __all__ = [
